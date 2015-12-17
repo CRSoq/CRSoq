554c4c83a824510988e46e8cc31728d6abe629ee
Yordan Vera <co2.acr@gmail.com>
2015-12-17 00:08:59 -0300

    #1 front, se agrega socket.io

---
+++ app/js/services/SocketServices.js
@@ -0,0 +1,16 @@
+'use strict';
+
+crsApp.factory('SocketServices', function ($rootScope) {
+    var socket = io();
+    return {
+        //conectar
+        on: function () {
+            
+        }
+        //ingresar a sala
+        //salir de sala
+        //abrir sala
+        //cerrar sala
+        
+    }
+});
