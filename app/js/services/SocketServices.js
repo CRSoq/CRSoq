7be588c681c30ca1e4f4f86ff40a5da642366060
Yordan Vera <co2.acr@gmail.com>
2016-01-27 11:18:55 -0300

    #1 front, modificación al servicio que controla el socket, se agregan las rutinas on, emit, connect, y getsocket (para el manejo del socket)

--- app/js/services/SocketServices.js
+++ app/js/services/SocketServices.js
@@ -1,16 +1,29 @@
 'use strict';
 
 crsApp.factory('SocketServices', function ($rootScope) {
-    var socket = io();
+    var socket = null;
     return {
-        //conectar
-        on: function () {
-            
+        on: function (eventName, callback) {
+            if(socket!=null){
+                socket.on(eventName, function () {
+                    var args = arguments;
+                    $rootScope.$apply(function () {
+                        callback.apply(socket, args);
+                    });
+                });
+            }
+        },
+        emit: function (eventName, data) {
+            if(socket!=null) {
+                socket.emit(eventName, data);
+            }
+        },
+        connect: function () {
+            socket = io.connect();
+            return socket;
+        },
+        getSocket: function () {
+            return socket;
         }
-        //ingresar a sala
-        //salir de sala
-        //abrir sala
-        //cerrar sala
-        
-    }
+    };
 });
