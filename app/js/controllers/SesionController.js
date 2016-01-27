1fe59d29b5386c5d5317e86def21f2eec349e328
Yordan Vera <co2.acr@gmail.com>
2016-01-27 11:48:11 -0300

    #1 front, - se eliminan líneas comentadas. - se agregan variables de control para la vista. - se agregan las funciones base para el manejo de la sesión con el manejo de socket.

--- app/js/controllers/SesionController.js
+++ app/js/controllers/SesionController.js
@@ -1,31 +1,19 @@
-crsApp.controller('SesionController', function($scope, $state, $stateParams, SesionClasesService, CursosServices, ClasesServices, PreguntasServices){
+crsApp.controller('SesionController', function($scope, $rootScope, $state, $stateParams, $timeout, SessionServices, CursosServices, ClasesServices, PreguntasServices, SocketServices){
     $scope.curso = $stateParams.curso;
-    //$scope.id_sesion = $stateParams.id_sesion;
-    /*
-    $scope.curso = $stateParams.curso;
-
-    var curso = CursosServices.getCursoPorNombre($stateParams.semestre, $stateParams.curso);
-    var sesion = {
-        'id_sesion' : $stateParams.id_sesion
-    };
-    ClasesServices.obtenerClasePorIDSesion(sesion).then(function (data) {
-        if(!data.error){
-            $scope.clase = data.descripcion;
-        }else{
-            //error
-        }
-    });
-    SesionClasesService.obtenerSesionPreguntas(sesion).then(function (data) {
-        if(!data.error){
-            $scope.sesion = data;
-        }else{
-            console.log('error al obtener sesion: '+data.err);
-        }
-    });
-    */
     //obtener preguntas de la clase
     $scope.pregunta = null;
     $scope.listaPreguntasClase=[];
+    $scope.listaParticipantes=[];
+
+    //view control
+    $scope.esperar = true; //mostar esperar por la pregunta
+    $scope.preguntaRealizada = false; //mostrar pregunta hecha por el profesor
+    $scope.participar = false; //mostrar boton participar
+    $scope.participantes = false; //mostrar lista de participantes
+    $scope.responder = false; //mostrar cuando le toca responder
+    $scope.resultado = false; //mostrar si esta correcta o no la respuesta
+
+    $scope.preguntaFinalizada = false;
     PreguntasServices.obtenerPreguntasClase({'id_clase':$stateParams.id_clase}).then(function (data) {
         if(data.error){
             console.log(data.err.code);
@@ -47,8 +35,14 @@ crsApp.controller('SesionController', function($scope, $state, $stateParams, Ses
     };
     //boton lanzar pregunta
     $scope.lanzarPregunta = function (pregunta) {
-        $scope.pregunta = pregunta.pregunta;
+
         $state.transitionTo('crsApp.cursosSemestre.clases.sesion.pregunta',{semestre:$stateParams.semestre,curso:$stateParams.curso,id_clase:$stateParams.id_clase,id_pregunta:pregunta.id_pregunta});
+        var data = {
+            'pregunta'  : pregunta,
+            'sala'      : $stateParams.semestre+$stateParams.curso+$stateParams.id_clase
+        };
+        $scope.pregunta = pregunta;
+        SocketServices.emit('RealizarPregunta', data);
     };
     //boton editar ganador
     $scope.editarGanadorPregunta = function (pregunta) {
@@ -56,10 +50,140 @@ crsApp.controller('SesionController', function($scope, $state, $stateParams, Ses
     };
     //boton finalizar sesion
     $scope.finalizarSesion = function () {
-
+        //emit fin de sesion
+        //  cambiar estado en el front
+        //cambiar estado en la bd
+        //mover de la ruta
     };
     //boton proyectar sesion
     $scope.proyectarSesion = function () {
 
     };
+
+    $scope.cerrarParticipacion = function () {
+        //emit fin de la pregunta
+        SocketServices.emit('cerrarParticipacion');
+        //activar opcion para seleccionar
+        $scope.preguntaFinalizada = true;
+    };
+
+    $scope.finalizarPregunta = function () {
+        //
+        $rootScope.$emit('continuarSesionPreguntas');
+        $state.transitionTo('crsApp.cursosSemestre.clases.sesion', {semestre:$stateParams.semestre,curso:$stateParams.curso,id_clase:$stateParams.id_clase});
+    };
+
+    //boton responder pregunta
+    $scope.responderPregunta = function () {
+        var dataUsuario = SessionServices.getSessionData();
+        SocketServices.emit('responderPregunta', dataUsuario);
+        $scope.participar = false;
+        $scope.participantes = true;
+
+    };
+
+    SocketServices.on('Pregunta', function (data) {
+        $scope.pregunta = data;
+        $scope.esperar = false;
+        $scope.participar = true;
+        $scope.preguntaRealizada = true;
+    });
+
+    $rootScope.$on('agregarEstudiante', function (event, data) {
+        if(_.findIndex($scope.listaParticipantes,{rut:data.rut})<0){
+            $scope.listaParticipantes.push(data);
+            SocketServices.emit('actualizarListaParticipantes', $scope.listaParticipantes);
+        }
+    });
+
+    SocketServices.on('listaParticipantes', function (listaParticipantes) {
+        $scope.listaParticipantes = listaParticipantes;
+    });
+
+
+    $rootScope.$on('finParticipacionEstudiantes', function (event, data) {
+        $scope.esperar = false;
+        $scope.participar = false;
+        $scope.preguntaRealizada = true;
+        $scope.participantes = true;
+    });
+
+    $scope.seleccionarEstudiante = function (participante) {
+        $scope.estudianteSeleccionado = participante;
+        SocketServices.emit('responderParticipante', participante);
+        $scope.responder = true;
+    };
+
+    $rootScope.$on('turnoParaResponder', function (event, data) {
+        $scope.esperar = false;
+        $scope.participar = false;
+        $scope.preguntaRealizada = true;
+        $scope.participantes = false;
+        $scope.responder = true;
+    });
+
+    $scope.respuestaCorrecta = function (pregunta) {
+        var data = {
+            'pregunta'  : pregunta,
+            'id_user'   : $scope.estudianteSeleccionado.id_user
+        };
+        //para estudiantes cambiar estado a esperar por pregunta
+        SocketServices.emit('respuestaCorrecta', $scope.estudianteSeleccionado);
+        //update bd,$scope.estudianteSeleccionado.id_user add to table pregunta where id_pregunta = pregunta
+
+        PreguntasServices.asignarGanador(data).then(function (data) {
+            if(data){
+                $rootScope.$emit('continuarSesionPreguntas');
+                $state.transitionTo('crsApp.cursosSemestre.clases.sesion', {semestre:$stateParams.semestre,curso:$stateParams.curso,id_clase:$stateParams.id_clase});
+            }
+        });
+        //cambiar ruta profesor, volver a preguntas
+
+
+    };
+
+    $scope.respuestaIncorrecta = function () {
+        //volver a mostrar la lista profesor
+        $scope.responder = false;
+        //notificar al estudiante del resultado
+        $scope.estudianteSeleccionado.seleccionado = true;
+        SocketServices.emit('respuestaIncorrecta', $scope.estudianteSeleccionado);
+        $scope.estudianteSeleccionado = null;
+    };
+
+    $rootScope.$on('mostrarResultadoIncorrecto', function (event, data) {
+        $scope.esperar = false;
+        $scope.participar = false;
+        $scope.preguntaRealizada = true;
+        $scope.participantes = false;
+        $scope.responder = false;
+        $scope.resultado = true;
+        $scope.msjResultado = "Lo siento, tu respuesta no es correcta.";
+    });
+    $rootScope.$on('mostrarResultadoCorrecto', function (event, data) {
+        $scope.esperar = false;
+        $scope.participar = false;
+        $scope.preguntaRealizada = true;
+        $scope.participantes = false;
+        $scope.responder = false;
+        $scope.resultado = true;
+        $scope.msjResultado = "Tu respuesta no es correcta !!";
+    });
+    $rootScope.$on('actualizarEstadoLista', function (event, data) {
+        var participante = _.findWhere($scope.listaParticipantes, {'usuario':data.usuario});
+        participante.seleccionado = true;
+    });
+    $rootScope.$on('continuarSesionPreguntas', function (event, data) {
+        $scope.pregunta = null;
+        $scope.listaParticipantes=null;
+
+        //view control
+        $scope.esperar = true; //mostar esperar por la pregunta
+        $scope.preguntaRealizada = false; //mostrar pregunta hecha por el profesor
+        $scope.participar = false; //mostrar boton participar
+        $scope.participantes = false; //mostrar lista de participantes
+        $scope.responder = false; //mostrar cuando le toca responder
+        $scope.resultado = false; //mostrar si esta correcta o no la respuesta
+        $scope.preguntaFinalizada = false;
+    });
 });
