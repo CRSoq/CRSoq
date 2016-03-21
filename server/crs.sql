df2fa5564a75ca62460ea309e10e124c7e668cce
Yordan Vera <co2.acr@gmail.com>
2016-03-21 01:04:38 -0300

    #1 actualización base de datos.

--- server/crs.sql
+++ server/crs.sql
@@ -1,6 +1,6 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     27/11/2015 14:35:41                          */
+/* Created on:     21-03-2016 1:01:47                           */
 /*==============================================================*/
 
 
@@ -47,7 +47,7 @@ create table administrador
    id_user              int not null auto_increment,
    usuario              varchar(250) not null,
    clave                varchar(250) not null,
-   token                varchar(65),
+   token                varchar(129),
    primary key (id_user),
    unique key ak_key_2 (usuario)
 );
@@ -90,7 +90,7 @@ create table estudiante
    apellido             varchar(250),
    usuario              varchar(250) not null,
    clave                varchar(250) not null,
-   token                varchar(65),
+   token                varchar(129),
    primary key (id_user),
    unique key ak_key_2 (usuario)
 );
@@ -145,7 +145,8 @@ create table pregunta
    id_pregunta          int not null auto_increment,
    id_curso             int not null,
    id_clase             int,
-   id_user              int not null,
+   id_user              int,
+   estado               varchar(50),
    pregunta             varchar(1024),
    primary key (id_pregunta)
 );
@@ -160,7 +161,7 @@ create table profesor
    apellido             varchar(250),
    usuario              varchar(250) not null,
    clave                varchar(250) not null,
-   token                varchar(65),
+   token                varchar(129),
    primary key (id_user),
    unique key ak_key_2 (usuario)
 );
@@ -176,47 +177,47 @@ create table puede_ganar
 );
 
 alter table actividad add constraint fk_se_realizan foreign key (id_clase)
-      references clase (id_clase) on delete restrict on update restrict;
+references clase (id_clase) on delete restrict on update restrict;
 
 alter table clase add constraint fk_contiene foreign key (id_modulo)
-      references modulo (id_modulo) on delete restrict on update restrict;
+references modulo (id_modulo) on delete restrict on update restrict;
 
 alter table curso add constraint fk_realiza foreign key (id_user)
-      references profesor (id_user) on delete restrict on update restrict;
+references profesor (id_user) on delete restrict on update restrict;
 
 alter table modulo add constraint fk_se_compone foreign key (id_curso)
-      references curso (id_curso) on delete restrict on update restrict;
+references curso (id_curso) on delete restrict on update restrict;
 
 alter table participa add constraint fk_participa foreign key (id_user)
-      references estudiante (id_user) on delete restrict on update restrict;
+references estudiante (id_user) on delete restrict on update restrict;
 
 alter table participa add constraint fk_participa2 foreign key (id_actividad)
-      references actividad (id_actividad) on delete restrict on update restrict;
+references actividad (id_actividad) on delete restrict on update restrict;
 
 alter table participan_por_responder add constraint fk_participan_por_responder foreign key (id_user)
-      references estudiante (id_user) on delete restrict on update restrict;
+references estudiante (id_user) on delete restrict on update restrict;
 
 alter table participan_por_responder add constraint fk_participan_por_responder2 foreign key (id_pregunta)
-      references pregunta (id_pregunta) on delete restrict on update restrict;
+references pregunta (id_pregunta) on delete restrict on update restrict;
 
 alter table pertenece add constraint fk_pertenece foreign key (id_user)
-      references estudiante (id_user) on delete restrict on update restrict;
+references estudiante (id_user) on delete restrict on update restrict;
 
 alter table pertenece add constraint fk_pertenece2 foreign key (id_curso)
-      references curso (id_curso) on delete restrict on update restrict;
+references curso (id_curso) on delete restrict on update restrict;
 
 alter table pregunta add constraint fk_gana foreign key (id_user)
-      references estudiante (id_user) on delete restrict on update restrict;
+references estudiante (id_user) on delete restrict on update restrict;
 
 alter table pregunta add constraint fk_posee foreign key (id_curso)
-      references curso (id_curso) on delete restrict on update restrict;
+references curso (id_curso) on delete restrict on update restrict;
 
 alter table pregunta add constraint fk_se_hacen foreign key (id_clase)
-      references clase (id_clase) on delete restrict on update restrict;
+references clase (id_clase) on delete restrict on update restrict;
 
 alter table puede_ganar add constraint fk_puede_ganar foreign key (id_user)
-      references estudiante (id_user) on delete restrict on update restrict;
+references estudiante (id_user) on delete restrict on update restrict;
 
 alter table puede_ganar add constraint fk_puede_ganar2 foreign key (id_actividad)
-      references actividad (id_actividad) on delete restrict on update restrict;
+references actividad (id_actividad) on delete restrict on update restrict;
 
