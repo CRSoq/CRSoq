d207f1e8b4b9894bc5970ef55f467d40889cce90
Classroom Response System <crs@Socompa.decom.uta.cl>
2017-12-14 21:37:03 -0300

    Modificados parametros por defecto

--- server/config.js
+++ server/config.js
@@ -1,9 +1,9 @@
 module.exports.config = {
-    server_ip   : '146.83.109.226',  // ip del servidor
+    server_ip   : '',  // ip del servidor
     server_port : 3000,             // 3000 puerto recomendado
-    db_user     : 'root',           // usuario de la base de datos
-    db_pass     : 'maisql',       // clave del usuario de la base de datos
-    db_name     : 'crs',          // nombre de la base de datos
+    db_user     : '',           // usuario de la base de datos
+    db_pass     : '',       // clave del usuario de la base de datos
+    db_name     : '',          // nombre de la base de datos
     db_host     : 'localhost',      // ip o host de la base de datos
     db_port     : 3306              // puerto de la base de datos (3306 puerto por defecto en mysql)
 };
